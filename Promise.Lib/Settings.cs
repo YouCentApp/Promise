61ce97623524ace324922f749ded8415b611d1c0
Ark Fen <arkfen@gmail.com>
2024-06-21 00:17:16 -0400

    Api Url added to the Lib Settings

---
+++ Promise.Lib/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System.Environment;
+public class Settings
+{
+    private const string apiUrlDev = "https://localhost:5014";
+    private const string apiUrlProd = "https://api.youcent.app";
+    private const string prod = "Production";
+    private const string dev = "Development";
+    private static readonly string _env = GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+    public static string ApiUrl()
+    {
+        if (_env == prod)
+        {
+            return apiUrlProd;
+        }
+        if (_env == dev)
+        {
+            return apiUrlDev;
+        }
+        return string.Empty;
+    }
+}
