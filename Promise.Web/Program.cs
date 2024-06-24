51b6a069ea579bda2aa4bf16d861ac30b02dcdf0
Ark Fen <arkfen@gmail.com>
2024-06-23 23:45:36 -0400

    Not using https redirect for Web project during Development

--- Promise.Web/Program.cs
+++ Promise.Web/Program.cs
@@ -19,8 +19,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseHttpsRedirection();
+}
 
-app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
