61dda8aa2bb857012bf17528940ba548df69eaf2
Ark Fen <arkfen@gmail.com>
2024-06-25 15:19:17 -0400

    SessionState added and SignUp page is fully implemented.

---
+++ Promise.Lib/SessionState.cs
@@ -0,0 +1,8 @@
+﻿public class SessionState
+{
+    public long UserId { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? AccessToken { get; set; }
+    public bool IsAuthenticated => !string.IsNullOrEmpty(AccessToken);
+}
--- Promise.Native/MauiProgram.cs
+++ Promise.Native/MauiProgram.cs
@@ -18,6 +18,7 @@ public static class MauiProgram
 
 		builder.Services.AddScoped<IMyEnvironment, MyEnvironment>();
 		builder.Services.AddScoped<ISettings, Settings>();
+		builder.Services.AddScoped<SessionState>();
 
 #if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
