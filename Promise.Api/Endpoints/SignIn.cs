d722626889611b3d7d5ac99ab8514fd6e559ddb8
Ark Fen <arkfen@gmail.com>
2024-06-26 11:50:54 -0400

    SignIn endpoint refactoring

---
+++ Promise.Api/Endpoints/SignIn.cs
@@ -0,0 +1,70 @@
+﻿using Promise.Api;
+
+public static class SignIn
+{
+    public static async Task<IResult> Run(HttpContext context, string? secret)
+    {
+        try
+        {
+            using var db = context.RequestServices.GetRequiredService<PromiseDb>();
+            User? user = null;
+            try
+            {
+                user = await context.Request.ReadFromJsonAsync<User>();
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                MainLogger.LogError("Error reading user from signin request : " + ex);
+                return Results.Json(new { success = false, error = "Server error..." });
+            }
+            if (user is null || user.Login is null || user.Password is null ||
+                user.Login.Length < 1 || user.Password.Length < 1)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return Results.Json(new { success = false, error = "No data or wrong data provided" });
+            }
+            var dbUser = db.Users.FirstOrDefault(u => u.Login == user.Login);
+            if (dbUser is null || dbUser.Password is null || dbUser.Salt is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                return Results.Json(new { success = false, error = "User not found" });
+            }
+            var hash = Security.GetPasswordHash(user.Password, dbUser.Salt);
+            if (hash != dbUser.Password)
+            {
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                return Results.Json(new { auth = false, error = "Wrong password!" });
+            }
+
+            if (secret is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                MainLogger.LogError("No secret provided for JWT");
+                return Results.Json(new { success = false, error = "Server error..." });
+            }
+            var payload = new Dictionary<string, object>
+        {
+            { Security.PayLoadFieldLogin, user.Login },
+            { Security.PayLoadFieldAuth, true },
+            { Security.PayLoadFieldExp, Security.GetAccessTokenLifetimeSeconds() }
+        };
+            var token = Security.CreateBearerJwt(payload, secret);
+            context.Response.Headers.TryAdd(Security.AuthorizationHttpHeader, token);
+            context.Response.StatusCode = StatusCodes.Status202Accepted;
+            return Results.Json(new ApiResponseUser
+            {
+                Success = true,
+                Error = "",
+                Id = dbUser.Id,
+                Login = dbUser.Login
+            });
+        }
+        catch (Exception ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            MainLogger.LogError("Error signing in user : " + ex);
+            return Results.Json(new { success = false, error = "Server error..." });
+        }
+    }
+}
--- Promise.Api/Program.cs
+++ Promise.Api/Program.cs
@@ -60,9 +60,6 @@ else
 
 
 
-
-
-
 // MinVerSup endpoint. It checks if the version of the APP is supported.
 app.MapGet("/minversup", () =>
 {
@@ -73,74 +70,11 @@ app.MapGet("/minversup", () =>
 
 
 
-
-
-
 // SignIn endpoint
 app.MapPost("/signin", async (HttpContext context) =>
 {
-    try
-    {
-        using var db = context.RequestServices.GetRequiredService<PromiseDb>();
-        User? user = null;
-        try
-        {
-            user = await context.Request.ReadFromJsonAsync<User>();
-        }
-        catch (Exception ex)
-        {
-            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-            MainLogger.LogError("Error reading user from signin request : " + ex);
-            return Results.Json(new { success = false, error = "Server error..." });
-        }
-        if (user is null || user.Login is null || user.Password is null ||
-            user.Login.Length < 1 || user.Password.Length < 1)
-        {
-            context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            return Results.Json(new { success = false, error = "No data or wrong data provided" });
-        }
-        var dbUser = db.Users.FirstOrDefault(u => u.Login == user.Login);
-        if (dbUser is null || dbUser.Password is null || dbUser.Salt is null)
-        {
-            context.Response.StatusCode = StatusCodes.Status404NotFound;
-            return Results.Json(new { success = false, error = "User not found" });
-        }
-        var hash = Security.GetPasswordHash(user.Password, dbUser.Salt);
-        if (hash != dbUser.Password)
-        {
-            context.Response.StatusCode = StatusCodes.Status403Forbidden;
-            return Results.Json(new { auth = false, error = "Wrong password!" });
-        }
-        var secret = configuration["Jwt:Secret"];
-        if (secret is null)
-        {
-            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-            MainLogger.LogError("No secret provided for JWT");
-            return Results.Json(new { success = false, error = "Server error..." });
-        }
-        var payload = new Dictionary<string, object>
-        {
-            { Security.PayLoadFieldLogin, user.Login },
-            { Security.PayLoadFieldAuth, true },
-            { Security.PayLoadFieldExp, Security.GetAccessTokenLifetimeSeconds() }
-        };
-        var token = Security.CreateBearerJwt(payload, secret);
-        context.Response.Headers.TryAdd(Security.AuthorizationHttpHeader, token);
-        context.Response.StatusCode = StatusCodes.Status202Accepted;
-        return Results.Json(new ApiResponseUser
-        {
-            Success = true,
-            Error = "",
-            Id = dbUser.Id,
-            Login = dbUser.Login
-        });
-    }
-    catch (Exception ex)
-    {
-        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-        MainLogger.LogError("Error signing in user : " + ex);
-        return Results.Json(new { success = false, error = "Server error..." });
-    }
+    var secret = configuration["Jwt:Secret"];
+    return await SignIn.Run(context, secret);
 })
 .Accepts<User>("application/json", "User data for Sign In")
 .WithOpenApi();
