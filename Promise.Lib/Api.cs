0a76153c7f9124c9ca2896af8efa3573e4a8df8a
Ark Fen <arkfen@gmail.com>
2024-11-13 22:11:39 -0400

    DotNet 9 update

--- Promise.Lib/Api.cs
+++ Promise.Lib/Api.cs
@@ -1,5 +1,5 @@
 ﻿public static class Api
 {
-    public const string UrlDev = "http://localhost:5014";
+    public const string UrlDev = "http://localhost:5001";
     public const string UrlProd = "https://promiseapi.azurewebsites.net";
 }
