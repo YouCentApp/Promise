327915f7a1f4ce1834c895c9c4e63cfaef16ecc0
Ark Fen <arkfen@gmail.com>
2025-01-03 19:59:41 -0400

    Additional changes in Native project after moving to .Net 9

--- Promise.Native/App.xaml.cs
+++ Promise.Native/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace Promise.Native;
+﻿
+namespace Promise.Native;
 
 public partial class App : Application
 {
@@ -6,6 +7,11 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+
 	}
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
 }
