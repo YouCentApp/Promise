837245cb4a7efa2f0f8e50880adac4513cb01cda
Ark Fen <arkfen@gmail.com>
2025-01-03 20:41:31 -0400

    Local API port changed

--- Promise.Lib/Api.cs
+++ Promise.Lib/Api.cs
@@ -1,5 +1,5 @@
 ﻿public static class Api
 {
-    public const string UrlDev = "http://localhost:5011";
+    public const string UrlDev = "http://localhost:7800";
     public const string UrlProd = "https://promiseapi.azurewebsites.net";
 }
